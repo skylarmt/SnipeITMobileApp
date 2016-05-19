c47be0c77f25ffdc145bf6255c138b2726b728ac
Skylar <admin@netsyms.com>
2016-05-19 00:26:42 -0600

    Fix bug where dropdown labels selectable on Android, add build script

---
+++ build-android.sh
@@ -0,0 +1,8 @@
+cordova build --release android
+cd platforms/android/build/outputs/apk
+cp android-release-unsigned.apk android-release-signed.apk
+jarsigner -verbose -keystore ~/Documents/Projects/Certificates/Android/android.keystore android-release-signed.apk android
+jarsigner -verify -verbose -certs android-release-signed.apk
+rm android-release-signed-zipaligned.apk
+zipalign -v 4 android-release-signed.apk android-release-signed-zipaligned.apk
+
